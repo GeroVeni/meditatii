29110b593da4125f3b3424313cc1fb4b11df4821
GeroVeni <geroveni@gmail.com>
2020-10-12 13:32:53 +0100

    Add new assets

--- js/custom/rating.js
+++ js/custom/rating.js
@@ -1,6 +1,14 @@
 function getRatingImagePath(rating) {
-  if (rating == 10) return 'images/5-star-rating.png';
-  if (rating == 9) return 'images/4stars-and-a-half.png';
+  if (rating == 10) return 'images/5 star rating.png';
+  if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
+  // if (rating == 9)  return 'images/4stars-and-a-half.png';
   return 'images/4stars.png';
 }
 
