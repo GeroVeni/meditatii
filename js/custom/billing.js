3a89be36e5df88667188de3b5e50630f05a32998
42420950 <42420950+RTodoran9870@users.noreply.github.com>
2020-10-13 13:42:53 +0100

    Changes to the booking page so that it goes to billing

--- js/custom/billing.js
+++ js/custom/billing.js
@@ -31,7 +31,7 @@ function billing(){
                                 req.onreadystatechange = function(){
                                     if (this.readyState == 4 && this.status == 200){
                                         var data = JSON.parse(this.responseText);
-                                        console.log(data["formformUrl"]);
+                                        console.log(data["formUrl"]);
                                         window.location.replace(data.formUrl);
                                     }
                                 };
