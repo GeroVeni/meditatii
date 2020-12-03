efe2e47873add6746131fceab98f013f461656e8
GeroVeni <geroveni@gmail.com>
2020-12-03 03:08:47 +0200

    Load tutor data in dashboard

--- js/custom/dashboard.js
+++ js/custom/dashboard.js
@@ -1,6 +1,15 @@
-let userSettingsForm = document.getElementById('user-settings-form');
-let nameField = userSettingsForm['name'];
-let surnameField = userSettingsForm['surname'];
+const userSettingsForm = document.getElementById('user-settings-form');
+const nameField = userSettingsForm['name'];
+const surnameField = userSettingsForm['surname'];
+
+const tutorSettingsForm = document.getElementById('tutor-settings-form');
+const descriptionField = tutorSettingsForm['description'];
+const aboutMeField = tutorSettingsForm['about-me'];
+const aboutSessionsField = tutorSettingsForm['about-sessions'];
+const maxHoursField = tutorSettingsForm['max-hours'];
+const gradesField = tutorSettingsForm['grades'];
+const priceField = tutorSettingsForm['price'];
+const educationField = tutorSettingsForm['education'];
 
 // Set listener for user login
 firebase.auth().onAuthStateChanged(function (user) {
@@ -15,10 +24,21 @@ firebase.auth().onAuthStateChanged(function (user) {
   }
 });
 
-// Update field values
+// Update user field values
 function updateFields(content) {
   nameField.value = content.name;
   surnameField.value = content.surname;
+}
+
+// Update tutor field values
+function updateTutorFields(content) {
+  descriptionField.value = content.description;
+  aboutMeField.value = content.about_me;
+  aboutSessionsField.value = content.about_sessions;
+  maxHoursField.value = content.max_hours;
+  gradesField.value = JSON.stringify(content.grades);
+  priceField.value = content.price;
+  educationField.value = JSON.stringify(content.education);
 }
 
 // Load function for user settings
@@ -26,7 +46,17 @@ async function loadUserSettings(idToken) {
   const query = `?token=${idToken}`;
   let response = await fetch(API_ENDPOINT + '/users/me' + query);
   let result = await response.json();
-  updateFields(result[0]);
+  let user = result[0];
+  updateFields(user);
+  if (user.roles.includes('tutor')) {
+    // TODO: Disable tutor
+    // Load tutor settings
+    let username = user.username;
+    response = await fetch(API_ENDPOINT + '/tutors/' + username);
+    let tutor = await response.json();
+    updateTutorFields(tutor);
+  }
+  
 }
 
 // Update function for user settings
@@ -51,6 +81,29 @@ function onUpdateUserSettingsSubmit () {
   firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
     .then(idToken => updateUserSettings(idToken))
     .then(() => { console.log('user data updated'); })
-    .catch(err => { console.log(err); });
+    .catch(err => { console.error(err); });
   return false;
 }
+
+function openTab(evt, tabName, defaultTarget, not_redirect = false) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName('settings-tab');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  tablinks = document.getElementsByClassName('tab-link');
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' w3-red', ''); 
+  }
+  document.getElementById(tabName).style.display = 'block';
+  let target = defaultTarget;
+  if (evt) target = evt.currentTarget;
+  target.className += ' w3-red';
+  var url = window.location.href.split('?')[0];
+  if (!not_redirect) location.replace(url + '?tab=' + tabName);
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+let tab = urlParams.get('tab');
+if (!tab) tab = 'personal';
+openTab(null, tab, document.getElementById(tab + '-tab'), true);
