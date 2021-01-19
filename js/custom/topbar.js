4ec713fe780d3d4f4c561b2514428ecc16e15816
GeroVeni <geroveni@gmail.com>
2021-01-19 18:26:48 +0000

    webflow update

--- js/custom/topbar.js
+++ js/custom/topbar.js
@@ -1,19 +1,28 @@
 firebase.auth().onAuthStateChanged(function(user) {
-  var logout_bar = document.getElementById("logout-bar");
-  var login_bar = document.getElementById("login-bar");
+  var logout_bars = document.getElementsByClassName("logout-bar");
+  var login_bars = document.getElementsByClassName("login-bar");
 
   if (user) {
     // User signed in
     var displayName = user.email;
     var dashboard_button = document.getElementById("dashboard-button");
-    login_bar.style.display="none";
-    logout_bar.style.display="inline";
+    for (let i = 0; i < login_bars.length; ++i) { login_bars[i].style.display = "none"; }
+    for (let i = 0; i < logout_bars.length; ++i) { logout_bars[i].style.display = "inline"; }
   } else {
     // User signed out
-    login_bar.style.display="inline";
-    logout_bar.style.display="none";
+    for (let i = 0; i < login_bars.length; ++i) { login_bars[i].style.display = "inline"; }
+    for (let i = 0; i < logout_bars.length; ++i) { logout_bars[i].style.display = "none"; }
   }
 });
+
+function toggleSideBar() {
+  var x = document.getElementById('collapsible-side-bar');
+  if (x.className.indexOf('w3-show') == -1) {
+    x.className += ' w3-show';
+  } else {
+    x.className = x.className.replace(' w3-show', '');
+  }
+}
 
 function logout() {
   firebase.auth().signOut().then(function() {
